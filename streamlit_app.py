fe4daf049fccb86b988f5f61b84f97027cd217cf
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-01 00:59:38 +0000

    Add Filtering

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,44 @@ try:
     st.write(intensity_data.head())
 except Exception as e:
     st.error(f"Error loading data: {e}")
+
+# Show multiselect widget for teams
+teams = st.multiselect(
+    "Teams",
+    intensity_data['Team'].unique(),
+    intensity_data['Team'].unique()
+)
+
+# Show multiselect widget for leagues
+leagues = st.multiselect(
+    "Leagues",
+    intensity_data['League'].unique(),
+    intensity_data['League'].unique()
+)
+
+# Show multiselect widget for races
+races = st.multiselect(
+    "Race",
+    intensity_data['Race'].unique(),
+    intensity_data['Race'].unique()
+)
+
+# Show slider widget for intensity
+intensity = st.slider("Intensity", 0, 100, (0, 100))
+
+# Filter data based on widget input
+@st.cache_data
+def filter_data(data, teams, leagues, races, intensity_range):
+    return data[
+        (data["Team"].isin(teams)) & 
+        (data["League"].isin(leagues)) &
+        (data["Race"].isin(races)) &
+        (data["Dispersion Score"].between(intensity_range[0], intensity_range[1]))
+    ]
+
+try:
+    df_filtered = filter_data(intensity_data, teams, leagues, races, intensity)
+    st.write("Data filtered successfully!")
+    st.write(df_filtered.head())
+except Exception as e:
+    st.error(f"Error filtering data: {e}")
