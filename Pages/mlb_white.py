ca6c8b1eecd0eed674137e905cefed3895f507e7
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-03 22:03:56 +0000

    changes

--- Pages/mlb_white.py
+++ Pages/mlb_white.py
@@ -2,28 +2,27 @@ import streamlit as st
 import utils
 
 def app():
-    st.set_page_config(layout="wide", initial_sidebar_state="expanded")
-    st.title("White Baseball Fans Analysis")
+    utils.apply_common_styles()
 
-    df = utils.load_data("path_to_data/white_baseball_fans.parquet")
-    
-    st.sidebar.header("Filters")
+    st.sidebar.title("Fanflux Navigation")
+    page = st.sidebar.selectbox("Select a page", ["White Baseball Fans"])
+    st.title("White Baseball Fans")
+
+    df = utils.load_data("data/Fanflux_Intensity_MLB_White.parquet")
+
     team = st.sidebar.selectbox('Select a Team', ['Choose an option'] + sorted(df['Team'].unique()))
-    income_level = st.sidebar.selectbox('Select Income Levels', ['Choose an option'] + sorted(df.columns[df.columns.str.contains('[$]')]))
+    income_level = st.sidebar.selectbox('Select Income Levels', ['Choose an option'] + sorted(df['Income Levels'].unique()))
 
-    if team != 'Choose an option':
-        df = df[df['Team'] == team]
+    filtered_df = df[(df['Team'] == team) & (df['Income Levels'] == income_level)] if team != 'Choose an option' and income_level != 'Choose an option' else df
 
-    if income_level != 'Choose an option':
-        df = df[df[income_level] > 0]
+    gb = utils.create_grid_options(filtered_df)
+    grid_response = st.datatable(filtered_df, gridOptions=gb, height=500, width='100%', reload_data=True)
 
-    st.header("Fan Opportunity Map")
     m = utils.create_map()
-    color_key = {'Avid': 'red', 'Casual': 'orange', 'Convertible': 'green'}
-    utils.add_map_markers(m, df, 'Fandom Level', color_key)
+    utils.add_map_markers(m, filtered_df, 'Fandom Level', {
+        'Avid': 'red',
+        'Casual': 'blue',
+        'Convertible': 'green'
+    })
     st_folium(m, width=700, height=500)
 
-    st.header("Fan Data Table")
-    columns_to_display = ['Team', 'League', 'Neighborhood', 'Fandom Level', 'Race', 'Total Fans']
-    df_display = df[columns_to_display]
-    st.dataframe(df_display)
