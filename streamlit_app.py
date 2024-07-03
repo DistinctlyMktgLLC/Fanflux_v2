712c622e59c23b525a2bdaa6645b520c5f352aa4
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-03 22:02:58 +0000

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from multiapp import MultiApp
 import Pages.home as home
 import Pages.mlb_black as black
 import Pages.mlb_white as white
-import Pages.mlb_hispanic as hispanic  # Updated to the correct module name
+import Pages.mlb_hispanic as hispanic  # Updated to the correct module
 import Pages.mlb_aapi as aapi
 import Pages.mlb_americanindian as americanindian
 import Pages.mlb_asian as asian
@@ -12,12 +12,14 @@ st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
 app = MultiApp()
 
+# Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Black Baseball Fans", black.app)
 app.add_app("White Baseball Fans", white.app)
-app.add_app("Hispanic Baseball Fans", hispanic.app)  # Updated to the correct module name
-app.add_app("AAPI Baseball Fans", aapi.app)
-app.add_app("American Indian Baseball Fans", americanindian.app)
+app.add_app("Hispanic Baseball Fans", hispanic.app)
+app.add_app("Asian American/Pacific Islander Baseball Fans", aapi.app)
+app.add_app("American Indian/Alaska Native Baseball Fans", americanindian.app)
 app.add_app("Asian Baseball Fans", asian.app)
 
+# The main app
 app.run()
