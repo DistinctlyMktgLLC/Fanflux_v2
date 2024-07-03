83a20b078dca2ae318e2b0e4443ba984f55969a0
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-03 16:44:06 +0000

    master push

--- Pages/home.py
+++ Pages/home.py
@@ -9,13 +9,13 @@ def app():
 
         Where Data Tells the Story of Fandom
 
-        # Detailed Demographics
+        ## Detailed Demographics
         Uncover the age, race, and socio-economic status of fans from every team, league, and neighborhood.
 
-        # Fandom Levels
+        ## Fandom Levels
         Understand the intensity of fandom, from die-hard fans to casual viewers.
 
-        # Interactive Visualizations
+        ## Interactive Visualizations
         Dive into interactive maps and dynamic charts that bring the data to life.
         """
     )
