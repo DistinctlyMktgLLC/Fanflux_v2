13774e977cdc4f3e7843ae37654ce4413c9d04c3
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-03 22:04:30 +0000

    commit

--- multiapp.py
+++ multiapp.py
@@ -1,19 +1,13 @@
-import streamlit as st
-
 class MultiApp:
     def __init__(self):
         self.apps = []
 
     def add_app(self, title, func):
-        self.apps.append({
-            "title": title,
-            "function": func
-        })
+        self.apps.append({"title": title, "function": func})
 
     def run(self):
-        st.title("Fanflux Navigation")
-        app = st.selectbox(
-            '',
+        app = st.sidebar.selectbox(
+            'Fanflux Navigation',
             self.apps,
             format_func=lambda app: app['title'])
         app['function']()
