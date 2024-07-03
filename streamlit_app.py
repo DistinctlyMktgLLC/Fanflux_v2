dd833d42293f531317a857a6e8a630422d847e0b
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-03 20:10:59 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,17 @@
-import os
 import streamlit as st
-from multiapp import MultiApp
+
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+
 import Pages.home as home
 import Pages.mlb_aapi as aapi
 import Pages.mlb_americanindian as americanindian
 import Pages.mlb_asian as asian
 import Pages.mlb_black as black
 import Pages.mlb_white as white
-
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+from multiapp import MultiApp
 
 app = MultiApp()
 
-# Add all your application here
 app.add_app("Home", home.app)
 app.add_app("AAPI Baseball Fans", aapi.app)
 app.add_app("American Indian Baseball Fans", americanindian.app)
@@ -20,5 +19,4 @@ app.add_app("Asian Baseball Fans", asian.app)
 app.add_app("Black Baseball Fans", black.app)
 app.add_app("White Baseball Fans", white.app)
 
-# The main app
 app.run()
