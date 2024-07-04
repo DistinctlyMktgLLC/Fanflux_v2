35c8e2ef2aae7109e70e45cdb86d7242004f1d3e
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-04 06:04:14 +0000

    pushed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from multiapp import MultiApp
 import Pages.home as home
 import Pages.mlb_white as white
-# Add other imports as needed
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
@@ -11,7 +10,6 @@ app = MultiApp()
 # Add all your applications here
 app.add_app("Home", home.app)
 app.add_app("White Baseball Fans", white.app)
-# Add other apps here
 
 # The main app
 app.run()
