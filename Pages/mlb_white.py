d0ebeb2113eada1d03a692feda4021129abb5237
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-04 06:09:15 +0000

    pushed

--- Pages/mlb_white.py
+++ Pages/mlb_white.py
@@ -26,11 +26,6 @@ def calculate_total(df, fandom_level):
     numeric_df = filtered_df[income_levels]
     return numeric_df.sum().sum()
 
-avid_total = calculate_total(df, 'Avid')
-casual_total = calculate_total(df, 'Casual')
-convertible_total = calculate_total(df, 'Convertible Fans')
-
-# Create styled scorecards
 def create_scorecard(title, value, color):
     st.markdown(
         f"""
@@ -42,25 +37,32 @@ def create_scorecard(title, value, color):
         unsafe_allow_html=True
     )
 
-st.title("White Baseball Fans")
-create_scorecard("Avid Fans", avid_total, "#FF5733")
-create_scorecard("Casual Fans", casual_total, "#FFC300")
-create_scorecard("Convertible Fans", convertible_total, "#3498DB")
+def app():
+    df = load_data()
+    
+    avid_total = calculate_total(df, 'Avid')
+    casual_total = calculate_total(df, 'Casual')
+    convertible_total = calculate_total(df, 'Convertible Fans')
 
-# Create the map
-unique_fandom_levels = df['Fandom Level'].unique()
-colors = ["#FF5733", "#FFC300", "#3498DB"][:len(unique_fandom_levels)]
+    st.title("White Baseball Fans")
+    create_scorecard("Avid Fans", avid_total, "#FF5733")
+    create_scorecard("Casual Fans", casual_total, "#FFC300")
+    create_scorecard("Convertible Fans", convertible_total, "#3498DB")
 
-m = leafmap.Map(center=[40, -100], zoom=4)
-m.add_points_from_xy(
-    df,
-    x="US lon",
-    y="US lat",
-    color_column="Fandom Level",
-    icon_names=["gear", "map", "leaf"],
-    spin=True,
-    add_legend=True,
-    colors=colors
-)
+    unique_fandom_levels = df['Fandom Level'].unique()
+    colors = ["#FF5733", "#FFC300", "#3498DB"][:len(unique_fandom_levels)]
 
-m.to_streamlit(height=700)
+    m = leafmap.Map(center=[40, -100], zoom=4)
+    m.add_points_from_xy(
+        df,
+        x="US lon",
+        y="US lat",
+        color_column="Fandom Level",
+        icon_names=["gear", "map", "leaf"],
+        spin=True,
+        add_legend=True,
+        colors=colors
+    )
+
+    m.to_streamlit(height=700)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from multiapp import MultiApp
 import Pages.home as home
+import Pages.mlb_aapi as aapi
+import Pages.mlb_americanindian as americanindian
+import Pages.mlb_asian as asian
+import Pages.mlb_black as black
+import Pages.mlb_hispanic as hispanic
 import Pages.mlb_white as white
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
@@ -9,6 +14,11 @@ app = MultiApp()
 
 # Add all your applications here
 app.add_app("Home", home.app)
+app.add_app("AAPI Baseball Fans", aapi.app)
+app.add_app("American Indian Baseball Fans", americanindian.app)
+app.add_app("Asian Baseball Fans", asian.app)
+app.add_app("Black Baseball Fans", black.app)
+app.add_app("Hispanic Baseball Fans", hispanic.app)
 app.add_app("White Baseball Fans", white.app)
 
 # The main app
