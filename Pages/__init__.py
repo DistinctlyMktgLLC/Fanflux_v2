52f94a27ca749054355eb206b43c7c31381abf68
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-08 20:22:07 +0000

    commits

---
+++ Pages/__init__.py
@@ -0,0 +1,11 @@
+# Pages/__init__.py
+
+# Import all modules
+from .home import app as home_app
+from .mlb_aapi import app as mlb_aapi_app
+from .mlb_americanindian import app as mlb_americanindian_app
+from .mlb_asian import app as mlb_asian_app
+from .mlb_black import app as mlb_black_app
+from .mlb_hispanic import app as mlb_hispanic_app
+from .mlb_white import app as mlb_white_app
+from .chatbot_page import app as chatbot_page_app
