34a6b6cc1580ec829e5b384aeefd6d5cb6b2925e
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-09 00:09:14 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
 import streamlit as st
 import sidebar_menu
 
-# Set up the page title
-st.set_page_config(page_title="Fanflux Dashboard")
-
-# Call the sidebar menu function
 page_function = sidebar_menu.sidebar_menu()
-
-# Run the selected page function
-if page_function:
-    page_function()
+page_function()
