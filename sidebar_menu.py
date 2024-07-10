6f3828905eb5c9fa23ffbbeefcd3aa895d016455
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-10 18:21:11 +0000

    push

--- sidebar_menu.py
+++ sidebar_menu.py
@@ -3,29 +3,30 @@ from streamlit_option_menu import option_menu
 from Pages import home_app, chatbot_page_app, leagues_app
 
 def sidebar_menu():
-    selected = option_menu(
-        "Fanflux",
-        ["Home", "Leagues", "Chatbot"],
-        icons=["house", "megaphone", "robot"],
-        menu_icon="cast",
-        default_index=0,
-        orientation="vertical",
-        styles={
-            "container": {"padding": "5!important", "background-color": "#262730", "position": "fixed"},
-            "icon": {"color": "white", "font-size": "25px"},
-            "nav-link": {
-                "font-size": "20px",
-                "text-align": "left",
-                "margin": "0px",
-                "color": "white",
+    with st.sidebar:
+        selected = option_menu(
+            "Fanflux",
+            ["Home", "Leagues", "Chatbot"],
+            icons=["house", "megaphone", "robot"],
+            menu_icon="cast",
+            default_index=0,
+            orientation="vertical",
+            styles={
+                "container": {"padding": "5!important", "background-color": "#262730"},
+                "icon": {"color": "white", "font-size": "25px"},
+                "nav-link": {
+                    "font-size": "20px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "color": "white",
+                },
+                "nav-link-selected": {"background-color": "#1f77b4"},
             },
-            "nav-link-selected": {"background-color": "#1f77b4"},
-        },
-    )
+        )
 
-    if selected == "Home":
-        home_app()
-    elif selected == "Leagues":
-        leagues_app()
-    elif selected == "Chatbot":
-        chatbot_page_app()
+        if selected == "Home":
+            home_app()
+        elif selected == "Leagues":
+            leagues_app()
+        elif selected == "Chatbot":
+            chatbot_page_app()
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 from sidebar_menu import sidebar_menu
 
+# Ensure the sidebar menu is displayed
+st.sidebar.title("Fanflux")
 sidebar_menu()
