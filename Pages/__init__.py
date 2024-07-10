957086546ce45adaf8c167f63e2361d664f19a66
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-10 05:49:36 +0000

    push

--- Pages/__init__.py
+++ Pages/__init__.py
@@ -1,8 +1,3 @@
 from .home import app as home_app
-from .mlb_aapi import app as mlb_aapi_app
-from .mlb_american_indian import app as mlb_american_indian_app
-from .mlb_asian import app as mlb_asian_app
-from .mlb_black import app as mlb_black_app
-from .mlb_hispanic import app as mlb_hispanic_app
-from .mlb_white import app as mlb_white_app
 from .chatbot_page import app as chatbot_page_app
+from .leagues_page import app as leagues_page_app
