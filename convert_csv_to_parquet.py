a9b94f4cf948a0135e4625e19a6e7c8206783340
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-10 02:58:53 +0000

    push

--- convert_csv_to_parquet.py
+++ convert_csv_to_parquet.py
@@ -1,28 +1,33 @@
 import pandas as pd
-import os
 
-# Directory where the CSV files are located
-csv_dir = 'data'
-
-# Directory where the Parquet files will be saved
-parquet_dir = 'data'
-
-# List of CSV files to be converted
+# Load all individual CSV files into a single DataFrame
 csv_files = [
-    'Fanflux_Intensity_NFL_Black.csv',
-    'Fanflux_Intensity_NFL_Hispanic.csv',
-    'Fanflux_Intensity_NFL_White.csv'
+    "data/Fanflux_Intensity_MLB_AAPI.csv",
+    "data/Fanflux_Intensity_MLB_American_Indian.csv",
+    "data/Fanflux_Intensity_MLB_Asian.csv",
+    "data/Fanflux_Intensity_MLB_Black.csv",
+    "data/Fanflux_Intensity_MLB_Hispanic.csv",
+    "data/Fanflux_Intensity_MLB_White.csv",
+    "data/Fanflux_Intensity_NBA_AAPI.csv",
+    "data/Fanflux_Intensity_NBA_American Indian.csv",
+    "data/Fanflux_Intensity_NBA_Asian.csv",
+    "data/Fanflux_Intensity_NBA_Black.csv",
+    "data/Fanflux_Intensity_NBA_Hispanic.csv",
+    "data/Fanflux_Intensity_NBA_White.csv",
+    "data/Fanflux_Intensity_NFL_Black.csv",
+    "data/Fanflux_Intensity_NFL_Hispanic.csv",
+    "data/Fanflux_Intensity_NFL_White.csv",
+    "data/Fanflux_Intensity_NHL_AAPI.csv",
+    "data/Fanflux_Intensity_NHL_American_Indian.csv",
+    "data/Fanflux_Intensity_NHL_Asian.csv",
+    "data/Fanflux_Intensity_NHL_Black.csv",
+    "data/Fanflux_Intensity_NHL_Hispanic.csv",
+    "data/Fanflux_Intensity_NHL_White.csv",
+    # Add paths for NBA, NFL, NHL, MLS files as needed
 ]
 
-for csv_file in csv_files:
-    # Read the CSV file
-    df = pd.read_csv(os.path.join(csv_dir, csv_file))
-    
-    # Define the Parquet file name
-    parquet_file = csv_file.replace('.csv', '.parquet')
-    
-    # Write the DataFrame to a Parquet file
-    df.to_parquet(os.path.join(parquet_dir, parquet_file))
+df_list = [pd.read_csv(file) for file in csv_files]
+df_all = pd.concat(df_list, ignore_index=True)
 
-print("Conversion completed.")
-
+# Save the combined DataFrame to a Parquet file
+df_all.to_parquet("data/Fanflux_Intensity_All_Leagues.parquet")
