64758841f0d68801a6ca1355f56f32fa719e992d
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-10 08:19:45 +0000

    push

--- utils.py
+++ utils.py
@@ -16,9 +16,9 @@ def apply_common_styles():
     )
 
 def display_fan_demographics(df):
-    total_avid_fans = df[df['Fandom Level'] == 'Avid']['Intensity'].sum()
-    total_casual_fans = df[df['Fandom Level'] == 'Casual']['Intensity'].sum()
-    total_convertible_fans = df[df['Fandom Level'] == 'Convertible']['Intensity'].sum()
+    total_avid_fans = df[df['Fandom Level'] == 'Avid'].shape[0]
+    total_casual_fans = df[df['Fandom Level'] == 'Casual'].shape[0]
+    total_convertible_fans = df[df['Fandom Level'] == 'Convertible'].shape[0]
 
     st.metric(label="Total Avid Fans", value=total_avid_fans)
     st.metric(label="Total Casual Fans", value=total_casual_fans)
