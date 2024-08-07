875fecab0e41e1aca877fa5b10ea1de68d7f4bd9
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-08-07 02:42:38 +0000

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
-from Pages.leagues_analysis import app as Leagues_analysis
 
+# Set page configuration
 st.set_page_config(page_title="Fanflux", layout="wide")
 
+# Importing the leagues analysis app function
+from Pages.leagues_analysis import app as Leagues_analysis
+
+# Run the app function
 Leagues_analysis()
